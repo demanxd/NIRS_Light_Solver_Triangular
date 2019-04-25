88796c795c81f9514a0f438f1c65c25d0b625f45
demanxd <dvsh36@yandex.ru>
2019-04-26 02:04:14 +0300

    Greetings on board! Don't_panic!

--- main.cpp
+++ main.cpp
@@ -9,90 +9,106 @@ const static float tacos = Pi / 180;                //some mexicans
 
 
 
-
+///R'
 float Rs(int R, int l, int betta)
 {
     return sqrt(pow(R,2) + pow(l,2) - 2*R*l*cos(betta * tacos));
 };
 
-
+///tau
 float tau0(int R, int l, int betta)
 {
     return (-R/(c * (1 + cos(betta * tacos))));
 }
-
-
+//
 float tau1(int R, int l, int betta)
 {
     return ((-R/c - l*cos(betta * tacos))/(1-c*cos(betta * tacos)));
 }
 
 
-///what about tau??
+///tau final
 float t00(int R, int l, int betta)
 {
     return tau0(R, l, betta);
 };
-
+//
 float t01(int R, int l, int betta)
 {
     return tau1(R, l, betta);
 };
-
+//
 float t10(int R, int l, int betta)
 {
     return tau0(R, l, betta) + ((1/c)*(l + sqrt(pow(R,2)+pow(l,2)-2*R*l*cos(betta * tacos))-R));
 };
-
+//
 float t11(int R, int l, int betta)
 {
     return tau1(R,l,betta) + ((1/c)*(l + sqrt(pow(R,2)+pow(l,2)-2*R*l*cos(betta * tacos))-R));
 };
-
+//
 float t20(int R, int l, int betta)
 {
     return tau0(R, l, betta) + ((1/c)*(l+Rs(R, l, betta)));
 };
-
+//
 float t21(int R, int l, int betta)
 {
     return tau1(R,l,betta) + ((1/c)*(l+Rs(R, l, betta)));
 };
 
-
+///cos gamma
 float cosy(int R, int l, int betta)
 {
     return ((R - l*cos(betta*tacos))/Rs(R, l, betta));
 };
 
-
+///E block
 float E0(int R, int l, int betta)
 {
     return ( -((w)/2*Pi*R)*(1/tan(betta/2)) );
 };
-
-
+//
 float E1(int R, int l, int betta)
 {
     return ( -(2*(cosy(R,l,betta)/sin(betta * tacos))) );
 };
-
-
+//
 float E2(int R, int l, int betta)
 {
     return ( tan((betta * tacos)/2) );
 };
 
 
+///Greetings
+void Greetings()
+{
+    for (int i = 0; i < 70; ++i)
+        std::cout << "=";
+    std::cout << std::endl;
+    for (int i = 0; i < 5; ++i)
+        std::cout << " ";
+    std::cout << "Programm for solving field of triangular antennas parameters" << std::endl;
+    for (int i = 0; i < 27; ++i)
+        std::cout << " ";
+    std::cout << "You are welcome!" << std::endl;
+    for (int i = 0; i < 70; ++i)
+        std::cout << "=";
+    for (int i = 0; i < 5; ++i)
+        std::cout << "\n";
+    std::cout << std::endl;
+}
 
 
 
 
 
-using namespace std;
 
 int main()
 {
-    cout << cos(30 * tacos) << endl;
+    //Greetings();
+    //int R, l, betta;
+    std::cout << "Please, input" << std::endl;
     return 0;
 }
